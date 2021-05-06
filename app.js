77a47f2c95bceea74a3141a63d38ed944e3aa9f3
Asha Kumar <ashakumar90@gmail.com>
2021-05-06 17:02:19 -0400

    added the starting menu options

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-import inquirer from "inquirer"; 
+import inquirer from "inquirer";
+import figlet from "figlet"; 
 
-console.log ("Welcome to the employee tracker")
+
+
+function welcome(){
+    console.log(
+        figlet.textSync("EMS")
+    );
+    console.log("Welcome to the Employee Management System")
+    showMainMenu();
+}
+
+function showMainMenu(){
+    inquirer
+        .prompt([
+            {
+                type:"list",
+                name:"viewAction",
+                message:"Choose a view action:",
+                choices:[
+                    "View all employees",
+                    "View all managers",
+                    "View all departments",
+                    "View all roles",
+                ],
+            },
+        ])
+        // .then(async(response) => {
+        //     if (response.viewAction === "View all employees"){
+
+        //     }
+        // })
+}
+
+welcome();
